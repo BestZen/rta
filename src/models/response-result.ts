6199949de571c1f3b9f3213e32c6db859ca911f3
karyjan <karyjan@qq.com>
2016-12-22 17:29:22 +0800

    cancel merge

--- src/models/response-result.ts
+++ src/models/response-result.ts
@@ -1,16 +1,7 @@
-
 export class ResponseResult{
     constructor(public resutl:any,
         public status:string,
         public message:string){
 
     }
-}
-
-export interface Message {
-	from: string
-	to: string,
-	command: string,
-    code:string,
-    result:Object
 }
